76eeb9d37ac1dc3a932cbaf12835557e2d1f0fde
Sandeep Devrari <sandeepdevrari77@gmail.com>
2019-05-15 17:50:52 +0530

    setter added

--- Test_1.java
+++ Test_1.java
@@ -1,4 +1,11 @@
 public class Test_1{
   private int testNumber;
   private String testName;
+  
+  public void setTestNumber(int testNumber){
+      this.testNumber=testNumber;
+  }
+  public void setTestName(String testName){
+      this.testName=testName;
+  }
 }
