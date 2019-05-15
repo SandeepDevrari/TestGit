bf884ae54fcd1e86963b7494a4df367cea4b9878
Sandeep Devrari <sandeepdevrari77@gmail.com>
2019-05-15 17:35:21 +0530

    learning from telusko added

--- MainTest.java
+++ MainTest.java
@@ -2,5 +2,6 @@ public class MainTest{
   public static void main(String[] args){
     System.out.println("TEST GIT");
     System.out.println("Welcome here");
+    System.out.println("I'm learning from Telusko");
   }
 }
