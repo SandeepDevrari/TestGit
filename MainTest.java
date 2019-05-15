a8358ca2298e4dc8cfbc3dea1ad106924235544b
Sandeep Devrari <sandeepdevrari77@gmail.com>
2019-05-15 17:33:21 +0530

    add missing ; in welcome

--- MainTest.java
+++ MainTest.java
@@ -1,6 +1,6 @@
 public class MainTest{
   public static void main(String[] args){
     System.out.println("TEST GIT");
-    System.out.println("Welcome here")
+    System.out.println("Welcome here");
   }
 }
